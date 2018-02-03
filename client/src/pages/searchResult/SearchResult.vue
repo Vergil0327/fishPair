1a734a27fc8040bf39770e2c2defb960dd0f86b5
vergil <vergil@brandma.co>
2018-02-04 03:16:14 +0800

    Add Search Result Page & Example Data

---
+++ client/src/pages/searchResult/SearchResult.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-carousel hide-delimiters>
+    <v-carousel-item v-for="(user,i) in users" src="" :key="i">
+        <v-flex xs12>
+          <v-card>
+            <v-card-title primary-title>
+              <div>
+                <h3 class="headline mb-0">{{ user.firstName + ' ' + user.lastName }}</h3>
+                <div>E-Mail: {{ user.email }}</div>
+              </div>
+            </v-card-title>
+            <v-card-media
+              height="200px"
+              contain
+              :src="user.src"
+            ></v-card-media>
+          </v-card>
+        </v-flex>
+    </v-carousel-item>
+  </v-carousel>
+</template>
+
+<script>
+import alanPhotoSticker from '@/assets/photoStickers/alan.jpg';
+import lionelPhotoSticker from '@/assets/photoStickers/lionel.jpg';
+import vergilPhotoSticker from '@/assets/photoStickers/vergil.jpg';
+
+export default {
+  name: 'searchResult',
+  data: () => ({
+    users: [
+      { firstName: 'Vergil', lastName: 'Wang', email: 'test@example.com', src: vergilPhotoSticker },
+      { firstName: 'Lionel', lastName: 'Chen', email: 'test@example.com', src: lionelPhotoSticker },
+      { firstName: 'Alan', lastName: 'Yeh', email: 'test@example.com', src: alanPhotoSticker },
+    ],
+  }),
+};
+</script>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router';
 
 import HomePage from '@/pages/homePage/HomePage';
 import Profile from '@/pages/user/Profile';
+import SearchResult from '@/pages/searchResult/SearchResult';
 
 Vue.use(Router);
 
@@ -19,6 +20,11 @@ export default new Router({
       name: 'UserProfile',
       component: Profile,
       props: true,
+    },
+    {
+      path: '/search/results',
+      name: 'SearchResult',
+      component: SearchResult,
     },
   ],
 });
