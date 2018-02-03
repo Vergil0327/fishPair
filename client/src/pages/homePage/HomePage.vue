5e6fea60cc1a572e93dd3b6bd12bdae4b58e3bdb
vergil <vergil@brandma.co>
2018-02-04 01:24:24 +0800

    Add Search Input to HomePage (Only View)

--- client/src/pages/homePage/HomePage.vue
+++ client/src/pages/homePage/HomePage.vue
@@ -1,64 +1,76 @@
 <template>
-  <div class="hello">
-    <div v-if="didClientChooseType" class="text-xs-center">
-      <h1>{{ msg }}</h1>
-      <h2>Ecosystem</h2>
-      <ul>
-        <li><a href="http://router.vuejs.org/" target="_blank" rel="noopener">vue-router</a></li>
-        <li><a href="http://vuex.vuejs.org/" target="_blank" rel="noopener">vuex</a></li>
-        <li><a href="http://vue-loader.vuejs.org/" target="_blank" rel="noopener">vue-loader</a></li>
-        <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-      </ul>
-    </div>
-    <jumbotron v-if="!didClientChooseType" :title="slogan">
-      <v-container fill-height fluid>
-        <v-layout row wrap>
-          <v-flex xs6 @click.stop="choose('fisher')">
-            <v-card hover class="py-2 mr-2">
-              <v-card-media height="100%"></v-card-media>
-              <v-card-title primary-title>
-                <div>
-                  <h3 class="headline text-xs-center mb-0">Fisher</h3>
+  <v-container
+    class="text-xs-center" fluid
+  >
+    <h1>{{ msg }}</h1>
+    <v-layout v-if="didClientChooseType" row>
+      <v-flex xs9>
+        <v-text-field
+          v-model="searchInput"
+          label="Search All You Need"
+          class="input-group--focused"
+          prepend-icon="search"
+          single-line
+          dark
+        ></v-text-field>
+      </v-flex>
+      <v-flex xs3>
+        <v-select
+          :items="[{ text: 'Mobile' }]"
+          label="Mobile"
+          dark
+        ></v-select>
+      </v-flex>
+    </v-layout>
+    <v-layout>
+    </v-layout>
+      <jumbotron v-if="!didClientChooseType" :title="slogan">
+        <v-container fill-height fluid>
+          <v-layout row wrap>
+            <v-flex d-flex xs6 @click.stop="choose('fisher')">
+              <v-card d-flex hover class="py-2 mr-1">
+                <v-card-title primary-title>
                   <div>
-                    <v-flex xs12>
-                      <v-icon>check</v-icon><span>I need MSC certificate application assistance</span>
-                    </v-flex>
-                    <v-flex xs12>
-                      <v-icon>check</v-icon><span>I need suggestion on fishery practice</span>
-                    </v-flex>
-                    <v-flex xs12>
-                      <v-icon>check</v-icon><span>I can offer fishery experience</span>
-                    </v-flex>
+                    <h3 class="headline text-xs-center mb-0">Fisher</h3>
+                    <div class="text-xs-left">
+                      <v-flex xs12>
+                        <v-icon>check</v-icon><span>I need MSC certificate assistance</span>
+                      </v-flex>
+                      <v-flex xs12>
+                        <v-icon>check</v-icon><span>I need suggestion on fishery practice</span>
+                      </v-flex>
+                      <v-flex xs12>
+                        <v-icon>check</v-icon><span>I can offer fishery experience</span>
+                      </v-flex>
+                    </div>
                   </div>
-                </div>
-              </v-card-title>
-            </v-card>
-          </v-flex>
-          <v-flex xs6 @click.stop="choose('consultant')">
-            <v-card hover class="py-2 mr-2">
-              <v-card-media height="100%"></v-card-media>
-              <v-card-title primary-title>
-                <div>
-                  <h3 class="headline text-xs-center mb-0">Consultant</h3>
+                </v-card-title>
+              </v-card>
+            </v-flex>
+            <v-flex d-flex xs6 @click.stop="choose('consultant')">
+              <v-card d-flex hover class="py-2 mr-1">
+                <v-card-title primary-title>
                   <div>
-                    <v-flex xs12>
-                      <v-icon>check</v-icon><span>I do research on fishery</span>
-                    </v-flex>
-                    <v-flex xs12>
-                      <v-icon>check</v-icon><span>I can offer MSC certificate application assistance</span>
-                    </v-flex>
-                    <v-flex xs12>
-                      <v-icon>check</v-icon><span>I participate in FIP</span>
-                    </v-flex>
+                    <h3 class="headline text-xs-center mb-0">Consultant</h3>
+                    <div class="text-xs-left">
+                      <v-flex xs12>
+                        <v-icon>check</v-icon><span>I do research on fishery</span>
+                      </v-flex>
+                      <v-flex xs12>
+                        <v-icon>check</v-icon><span>I can offer MSC certificate assistance</span>
+                      </v-flex>
+                      <v-flex xs12>
+                        <v-icon>check</v-icon><span>I participate in FIP</span>
+                      </v-flex>
+                    </div>
                   </div>
-                </div>
-              </v-card-title>
-            </v-card>
-          </v-flex>
-        </v-layout>
-      </v-container>
-    </jumbotron>
-  </div>
+                </v-card-title>
+              </v-card>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </jumbotron>
+  </v-container>
 </template>
 
 <script>
@@ -78,9 +90,9 @@ export default {
   },
   data() {
     return {
-      msg: 'Search Bar Goes Here',
+      msg: 'Search All You Need',
       slogan: 'Are You A Fisher, or A Consultant ?',
-      // didClientChooseType: false,
+      searchInput: '',
     };
   },
   coumputed: {
@@ -99,7 +111,6 @@ export default {
     ]),
     choose(clientType) {
       this.fetchState({ userType: clientType });
-      // this.didClientChooseType = true;
       this.$emit('update:didClientChooseType', true);
     },
   },
